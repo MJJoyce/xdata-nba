596435963ae28cb748fa20a2cd4f5a756d45edee
Michael Joyce <mltjoyce@gmail.com>
2014-06-11 17:51:24 -0700

    Add JDK and Solr to Vagrant build

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -4,9 +4,13 @@
 set -e
 
 apt-get update
-apt-get install -y python python-dev maven curl git
-curl https://bootstrap.pypa.io/get-pip.py > get-pip.py
-python get-pip.py
-rm get-pip.py
+apt-get install -y unzip
+apt-get install -y python python-dev python-pip maven curl git
+apt-get install -y openjdk-7-jre-headless
+
 pip install -r /vagrant/requirements.txt
 python -m nltk.downloader book
+
+# Grab Solr
+wget http://mirror.reverse.net/pub/apache/lucene/solr/4.8.1/solr-4.8.1.zip
+unzip solr-4.8.1.zip
