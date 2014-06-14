9d8864eac44d14d303a00d92c30b34c8c29e8ab0
Michael Joyce <mltjoyce@gmail.com>
2014-06-13 20:27:20 -0700

    Add gora

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 
 # Stop if there is an error
-set -e
+#set -e
 
 apt-get update
-apt-get install -y unzip
+apt-get install -y curl
 apt-get install -y python python-dev python-pip maven git
 apt-get install -y openjdk-7-jre-headless
 
-pip install -r /vagrant/requirements.txt
-python -m nltk.downloader book
+## Grab the latest GORA code
+cd /home/vagrant
+git clone http://git.apache.org/gora.git/
+cd gora
+mvn clean compile
 
 # Grab and configure Solr
-wget -q http://mirror.reverse.net/pub/apache/lucene/solr/4.8.1/solr-4.8.1.zip
-unzip solr-4.8.1.zip
-cd solr-4.8.1.zip
+cd /home/vagrant
+curl -o solr-4.8.1.tgz http://mirror.reverse.net/pub/apache/lucene/solr/4.8.1/solr-4.8.1.tgz
+#unzip solr-4.8.1.zip
+tar xzf solr-4.8.1.tgz
+cd solr-4.8.1/example
 rm -rf solr
 ln -s /vagrant/solr ./solr
+
+pip install -r /vagrant/requirements.txt
+python -m nltk.downloader book
