8d5f367dc9bce383992b9a1409dce8b31053d830
Michael Joyce <mltjoyce@gmail.com>
2014-06-26 07:11:08 -0700

    Doc updates for NBA Ingest Controller

--- python-import/NBAIngest.py
+++ python-import/NBAIngest.py
@@ -44,27 +44,40 @@ def load_records(records_directory):
     logging.info('Data ingest complete for: ' + records_directory)
 
 def load_game_players(game_players_dir):
-    ''''''
+    '''Load GamePlayers data into a solr instance.
+
+    :param game_players_dir: Directory containing XDATA NBA GamePlayers JSON
+        JSOn files to load into a Solr instance.
+    '''
     logging.info('Starting GamePlayers ingestion: ' + game_players_dir)
 
+    # Get a list of all the files we need to load
     data_files = [os.path.join(game_players_dir, f)
                   for f in os.listdir(game_players_dir)
                   if os.path.isfile(os.path.join(game_players_dir, f))]
 
+    # Determine the number of threads it will take to process that many files
     total_threads = len(data_files) / GAME_PLAYER_FILES_PER_THREAD
+    # If the number of files isn't evenly divisible by the number of files
+    # per thread that we want to use we need to compensate for the remainder.
     total_threads += 1 if len(data_files) % GAME_PLAYER_FILES_PER_THREAD else 0
 
+    # Split the data files into chunks to pass to each thread.
     fpt = GAME_PLAYER_FILES_PER_THREAD
     split_data_files = [data_files[(fpt * index):(fpt * index) + fpt]
                         for index in range(total_threads)]
 
+    # Process all the files!
     thread_pool = multiprocessing.Pool(total_threads)
     thread_pool.map(load_game_players_files, split_data_files)
 
     logging.info('GamePlayers ingestions complete')
 
 def load_game_players_files(game_players_files):
-    ''''''
+    '''Load XDATA NBA GamePlayers files into Solr
+
+    :param game_players_files: List of GamePlayers files to load into Solr
+    '''
     solr_url = SOLR_URL + GAME_PLAYERS_CORE + 'update?commit=true'
 
     for f in game_players_files:
