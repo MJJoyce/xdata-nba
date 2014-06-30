fc94614bcc85b332e47b5320876933a67b810547
Tyler Palsulich <tpalsulich@gmail.com>
2014-06-30 09:15:54 -0700

    Use Java version 1.7 and quiet modes for downloading and installing.

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -10,17 +10,19 @@ sudo apt-get update
 #sudo apt-get install -y python-pip
 sudo apt-get install -y maven
 sudo apt-get install -y git
-sudo apt-get install -y oracle-java7-set-default
+sudo apt-get install -y openjdk-7-jdk
+
+update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
 
 # Grab the latest GORA code
 cd /home/vagrant
 git clone http://git.apache.org/gora.git/
 cd gora
-mvn clean install
+mvn -q clean install
 
 # Grab and configure Solr
 cd /home/vagrant
-wget http://mirror.reverse.net/pub/apache/lucene/solr/4.8.1/solr-4.8.1.tgz
+wget --quiet http://mirror.reverse.net/pub/apache/lucene/solr/4.8.1/solr-4.8.1.tgz
 tar xzf solr-4.8.1.tgz
 cd solr-4.8.1/example
 rm -rf solr
@@ -29,4 +31,4 @@ ln -s /vagrant/solr ./solr
 #pip install -r /vagrant/requirements.txt
 #python -m nltk.downloader book
 
-echo "JAVA_HOME=/usr" >> /home/vagrant/.bashrc
+echo "export JAVA_HOME=\$(readlink -f /usr/bin/java | sed \"s:bin/java::\")" >> /home/vagrant/.bashrc
