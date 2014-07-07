4489007c4d5f5be0744f256906e8d043f5315636
Michael Joyce <mltjoyce@gmail.com>
2014-07-07 07:26:06 -0700

    NLTK example now uses a test comment file

--- questions/nltk-example.py
+++ questions/nltk-example.py
@@ -1,9 +1,11 @@
-# This is not my original work. This was taken wholesale from
+# This is not my original work. This was taken (mostly) wholesale from
 # http://streamhacker.com/2010/05/10/text-classification-sentiment-analysis-naive-bayes-classifier/
 
 import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import movie_reviews
+
+TEST_COMMENT_FILE = '../test-data/comments/0021300632_espn_comments.txt'
  
 def word_feats(words):
     return dict([(word, True) for word in words])
@@ -14,13 +16,17 @@ posids = movie_reviews.fileids('pos')
 negfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in negids]
 posfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in posids]
  
-negcutoff = len(negfeats)*3/4
-poscutoff = len(posfeats)*3/4
+poscutoff = len(posfeats)
+negcutoff = len(negfeats)
  
 trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff]
 testfeats = negfeats[negcutoff:] + posfeats[poscutoff:]
-print 'train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats))
  
 classifier = NaiveBayesClassifier.train(trainfeats)
-print 'accuracy:', nltk.classify.util.accuracy(classifier, testfeats)
-classifier.show_most_informative_features()
+
+with open(TEST_COMMENT_FILE) as comment_file:
+    for line in comment_file:
+        user, comment = line.split('::')
+        print '-------'
+        #print classifier.classify(feature_extractor(comment)), comment
+        print classifier.classify(word_feats(comment)), comment
