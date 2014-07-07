de4f05eb369e8b0e8fa1db22b0ed1e0dee5a2548
Michael Joyce <mltjoyce@gmail.com>
2014-07-07 07:34:06 -0700

    Update GameComments parser so user comments aren't grouped
    - User comments are no longer grouped for each game_id. A unique entry
      is made for each user's comment per game.
    - Change id for GameComments table. Now we're using
      game_id_player_id_comment_number.

--- python-import/GameComments.py
+++ python-import/GameComments.py
@@ -17,20 +17,20 @@ def parse_comment_files(comment_file):
     records = defaultdict(list)
 
     with open(comment_file, 'r') as comment_in:
-        for line in comment_in:
+        for count, line in enumerate(comment_in):
             split_line = line.split('::')
             commenter_id = split_line[0]
             comment = split_line[1]
-            records[commenter_id].append(comment)
 
-    record = [{'id': str(game_id) + '_' + str(commenter_id),
-               'game_id': game_id,
-               'commenter': commenter_id,
-               'comments': ' '.join(records[commenter_id]),
-               'source': source}
-              for commenter_id in records]
+            records.append({
+                'id': str(game_id) + '_' + str(commenter_id) + '_' str(count)
+                'game_id': game_id,
+                'commenter': commenter_id,
+                'comments': comment
+                'source': source}
+            })
 
-    return record
+    return records
 
 if __name__ == '__main__':
     print parse_comment_files(sys.argv[1])
