80affd71e8f30d148756deee2ccf749b4f2445af
Michael Joyce <mltjoyce@gmail.com>
2014-07-24 07:26:11 -0700

    Comment, docs, and code clean up

--- questions/commentary_ne_extractor.py
+++ questions/commentary_ne_extractor.py
@@ -16,6 +16,13 @@ for r in r.json()['response']['docs']:
     commentary_data[r['game_id']] = r
 
 def detect_named_entities_in_commentary(game_id):
+    '''Retrieve named entities in commentary data for a specific game.
+
+    :param game_id: The game Id for which commentary data should be analyzed.
+    :type game_id: string
+
+    :returns: A set of named entities extracted from commentary data.
+    '''
     doc = commentary_data[game_id]
 
     prepared_preview = prep_for_named_entity_extraction(doc['preview'])
@@ -30,9 +37,24 @@ def detect_named_entities_in_commentary(game_id):
     return named_entities
 
 def prep_for_named_entity_extraction(string):
+    '''Prepare a string for NLTK-based named entity extraction.
+
+    :param string: The string to run NE extraction over.
+    :type string: string
+
+    :returns: The processed string which has been tokenized, part-of-speech
+        tagged, and ne chunked. The string is now in a Tree format that can
+        be traversed to extract Named Entities.
+    '''
     return nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(string)), binary=True)
 
 def extract_entity_names(tree):
+    '''Parse an NLTK ne_chunk returned Tree for NE tags.
+
+    Check the NLTK ne_chunk docs out for additional information on the
+    expected format.
+    http://www.nltk.org/api/nltk.chunk.html
+    '''
     entity_names = []
     
     if hasattr(tree, 'node') and tree.node:
@@ -43,11 +65,6 @@ def extract_entity_names(tree):
                 entity_names.extend(extract_entity_names(child))
                 
     return entity_names
-
-def run_query(query):
-    ''''''
-    r = requests.get(query)
-    return r.json()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='XDATA NBA NE Extractor')
